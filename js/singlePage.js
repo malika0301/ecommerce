e22962b5fddc518de33c407d2a4b17bd6c831c18
malika0301 <kr.serdce261086@mail.ru>
2025-11-07 19:42:38 +0500

    adding

--- js/singlePage.js
+++ js/singlePage.js
@@ -3,17 +3,20 @@ let id = path.get("id");
 let singleCards = document.getElementById("single-cards");
 
 let carts = JSON.parse(localStorage.getItem("carts") || "[]");
+let like = JSON.parse(localStorage.getItem("likes") || "[]");
+
 
 let item = products.filter((el) => el.id == id);
-let newCards = document.getElementById("new");
-let newFourProducts = products.slice(products.length - 4, products.length);
+let newsCards = document.getElementById("new");
+let newFoursProducts = products.slice(products.length - 4, products.length);
+
 
 function showSingleProducts(){
     singleCards.innerHTML = "";
     item.map((el) => {
         singleCards.innerHTML += `
     <div>
-                <h1 class="font-bold text-[50px]">${el.description}</h1>
+                <h4 class="font-bold text-[50px]">${el.description}</h4>
     </div>
     <div class="flex items-center w-[100px] gap-[15px] p-[10px]">
             <img class="w-[50px] object-cover" src="../images/sp3.png" alt="">
@@ -42,19 +45,19 @@ function showSingleProducts(){
                         <p class="text-gray-600 text-[clamp(16px,3vw,20px)] font-bold">С картой</p>
                         <p class="text-black-500 text-[clamp(16px,3vw,20px)] font-bold">${el.price - el.price * el.discount / 100}₽</p>
                     </div>
-                    
-                </div>
-                ${carts.find((cart) => cart.id === el.id) ? `
+                    ${carts.find((cart) => cart.id === el.id) ? `
                     <div class="w-full grid grid-cols-3 pt-[10px] gap-[10px]">
           <button
           onClick="decrease(${el.id})" class="bg-[green] p-[10px] rounded-[5px] flex items-center justify-center text-[18px] font-bold text-white">-</button>
-          <span class="bg-white p-[10px] flex items-center justify-center text-[18px] font-bold">${cart.find((item) => item.id === el.id).numbers}</span>
+          <span class="bg-white p-[10px] flex items-center justify-center text-[18px] font-bold">${carts.find((item) => item.id === el.id).numbers}</span>
           <button
           onClick ="increase(${el.id})" class="bg-[green] p-[10px] rounded-[5px] flex items-center justify-center text-[18px] font-bold text-white">+</button>
         </div>`
                 : `<button onClick="addToCart(${el.id})" class=" border border-green-500 bg-transparent text-green-600 w-full mt-3 py-2 rounded hover:text-white hover:bg-orange-600 transition duration-200">В корзину</button>
                 </div>`
             }
+                </div>
+                
                     <div>
                         <img class=" p-[10px] object-cover" src="../images/sp4.svg" alt="">
                     </div>
@@ -62,8 +65,11 @@ function showSingleProducts(){
             </div>`;
     });
 }
+showSingleProducts()
 
 let sideImages = document.getElementById("side-images");
+console.log(sideImages);
+
 let mainImg = document.getElementById("main-img");
 sideImages.addEventListener("click" , function(e){
     if(e.target.src){
@@ -73,24 +79,24 @@ sideImages.addEventListener("click" , function(e){
 function addToCart(id) {
     let item = products.find((el) => el.id === id)
     item.numbers = 1
-    cart.push(item);
-    badge.textContent = cart.length
-    localStorage.setItem("carts", JSON.stringify(cart));
+    carts.push(item);
+    badge.textContent = carts.length
+    localStorage.setItem("carts", JSON.stringify(carts));
     showSingleProducts();
 }
 function increase(id) {
-    cart = cart.map((item) => {
+    carts = carts.map((item) => {
         if (item.id === id) {
             item.numbers += 1
         }
         return item
     });
-    localStorage.setItem("carts", JSON.stringify(cart));
+    localStorage.setItem("carts", JSON.stringify(carts));
     showSingleProducts();
 }
 function decrease(id) {
-    let item = cart.find((el) => el.id === id)
-    cart = cart.map((item) => {
+    let item = carts.find((el) => el.id === id)
+    carts = carts.map((item) => {
         if (item.id === id) {
             item.numbers -= 1
         }
@@ -99,11 +105,11 @@ function decrease(id) {
     });
 
     if (item.numbers === 0) {
-        cart = cart.filter((el) => el.id !== id);
+        carts = carts.filter((el) => el.id !== id);
     }
 
-    localStorage.setItem("carts", JSON.stringify(cart));
-    badge.textContent = cart.length
+    localStorage.setItem("carts", JSON.stringify(carts));
+    badge.textContent = carts.length
     showSingleProducts();
 }
 function showProduct(content, data) {
@@ -123,83 +129,12 @@ function showProduct(content, data) {
                     
                 </div> 
                 <p class="mt-2 font-medium mb-5 line-clamp-2">${el.description}</p>
-                 ${el.rating === 5 ? `
-                    <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    </div>
-                    ` : el.rating === 4.5 ? `
-                    <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star yarm.svg" alt="">
-                    </div>`
-                : el.rating === 4 ? `
-                <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                 </div>`
-                    : el.rating === 3.5 ? `
-                 <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star yarm.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                 </div>`
-                        : el.rating === 3 ? `
-                <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                 </div>`
-                            : el.rating === 2.5 ? `
-                <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star yarm.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                 </div>`
-                                : el.rating === 2 ? `
-                 <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    </div>`
-                                    : el.rating === 1.5 ? `
-                    <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star yarm.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    </div>`
-                                        : el.rating === 1 ? `
-                    <div class="flex justify-between w-27 h-5 mb-5">
-                    <img class="h-5 w-5" src="./images/star.svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    <img class="h-5 w-5" src="./images/star (1).svg" alt="">
-                    </div>`: ""}
-                       ${cart.find((item) => item.id === el.id) ? `
+                 
+                       ${carts.find((item) => item.id === el.id) ? `
          <div class="w-full grid grid-cols-3 gap-[10px]">
           <button 
           onClick="decrease(${el.id})" class="bg-[green] p-[10px] rounded-[5px] flex items-center justify-center text-[18px] font-bold text-white">-</button>
-          <span class="bg-white p-[10px] flex items-center justify-center text-[18px] font-bold">${cart.find((item) => item.id === el.id).numbers}</span>
+          <span class="bg-white p-[10px] flex items-center justify-center text-[18px] font-bold">${carts.find((item) => item.id === el.id).numbers}</span>
           <button
           onClick ="increase(${el.id})" class="bg-[green] p-[10px] rounded-[5px] flex items-center justify-center text-[18px] font-bold text-white">+</button>
         </div>`
@@ -210,4 +145,4 @@ function showProduct(content, data) {
     });
 };
 
-showProduct(newCards, newFourProducts);
+showProduct(newsCards, newFoursProducts);
